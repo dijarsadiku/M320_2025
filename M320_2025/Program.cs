204a5938398f1cc3aa4d6b4db913476cb1652b4b
dijar.sadiku <dijar.sadiku@ksb-sg.ch>
2025-08-18 09:43:05 +0200

    Add project files.

---
+++ M320_2025/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace M320_2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int a = 2;
+            int b = 3;
+            int summe = mymath.Add(a, b);
+            Console.WriteLine($"Summe von {a} und {b} = {summe}");
+        }
+    }
+}
---
+++ M320_2025/mymath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M320_2025
+{
+    internal class mymath
+    {
+        static internal int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
